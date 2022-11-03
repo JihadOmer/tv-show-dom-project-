cbf875d57ef5b813614685cf74a646f835219689
Jihad Omer <jhjahony@gmail.com>
2022-11-03 11:30:03 +0000

    header and search bar created

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  makePageForEpisodes(allEpisodes)
+ 
 }
+
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
